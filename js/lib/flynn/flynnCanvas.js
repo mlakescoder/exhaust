3feebd02c4171bb9c02c67564d94ed570a0afd80
Eric Moyer <eric@lemoncrab.com>
2015-05-16 19:50:23 -0700

    Refactor vector graphics simulation.

--- js/lib/flynn/flynnCanvas.js
+++ js/lib/flynn/flynnCanvas.js
@@ -3,6 +3,8 @@ var FlynnMaxPaceRecoveryTicks = 5; // Max elapsed 60Hz frames to apply pacing (b
 var FlynnTextCenterOffsetX = FlynnCharacterWidth/2;
 var FlynnTextCenterOffsetY = FlynnCharacterHeight/2;
 
+var FlynnVectorDimFactor = 0.6;
+
 var FlynnCanvas = Class.extend({
 
 	init: function(game, width, height) {
@@ -72,23 +74,47 @@ var FlynnCanvas = Class.extend({
 				this.stroke();
 			};
 
-			//----- WILL DEPRECATE THESE TWO VERY SHORTLY
-			ctx.moveToVec = function(x,y){
-				this.moveTo(Math.floor(x)+0.5, Math.floor(y)+0.5);
+			//-----------------------------
+			// Vector graphic simulation
+			//-----------------------------
+			ctx.vectorStart = function(color){
+				var dim_color_rgb = flynnHexToRgb(color);
+				dim_color_rgb.r *= FlynnVectorDimFactor;
+				dim_color_rgb.g *= FlynnVectorDimFactor;
+				dim_color_rgb.b *= FlynnVectorDimFactor;
+				var dim_color = flynnRgbToHex(dim_color_rgb.r, dim_color_rgb.g, dim_color_rgb.b);
+
+				this.vectorVericies = [];
+				this.vectorVertexColor = color;
+				this.beginPath();
+				this.strokeStyle = dim_color;
+				//this.lineWidth = "6"; // Fat lines for screenshot thumbnail generation
 			};
 
-			ctx.lineToVec = function(x,y){
-				this.lineTo(Math.floor(x)+0.5, Math.floor(y)+0.5);
+			ctx.vectorLineTo = function(x, y){
+				x = Math.floor(x);
+				y = Math.floor(y);
+				this.vectorVericies.push(x, y);
+				this.lineTo(x+0.5, y+0.5);
 			};
-			//--------------------------------------------
 
-			// ctx.moveToVec = function(x,y){
-			// 	this.moveTo(Math.floor(x)+0.5, Math.floor(y)+0.5);
-			// };
+			ctx.vectorMoveTo = function(x, y){
+				x = Math.floor(x);
+				y = Math.floor(y);
+				this.vectorVericies.push(x, y);
+				this.moveTo(x+0.5, y+0.5);
+			};
 
-			// ctx.lineToVec = function(x,y){
-			// 	this.lineTo(Math.floor(x)+0.5, Math.floor(y)+0.5);
-			// };
+			ctx.vectorEnd = function(){
+				// Finish the line drawing 
+				this.stroke();
+
+				// Draw the (bright) vector vertex points
+				this.fillStyle = this.vectorVertexColor;
+				for(var i=0, len=this.vectorVericies.length; i<len; i+=2) {
+					ctx.fillRect(this.vectorVericies[i], this.vectorVericies[i+1], 1, 1);
+				}
+			};
 
 			ctx.vectorText = function(text, scale, x, y, offset, color){
 				if(typeof(color)==='undefined'){
@@ -112,17 +138,6 @@ var FlynnCanvas = Class.extend({
 					y = Math.round((this.height - step)/2);
 				}
 
-				x += 0.5;
-				y += 0.5;
-
-				var dim_color_rgb = flynnHexToRgb(color);
-				var dim_factor = 0.6;
-				dim_color_rgb.r *= dim_factor;
-				dim_color_rgb.g *= dim_factor;
-				dim_color_rgb.b *= dim_factor;
-				var dim_color = flynnRgbToHex(dim_color_rgb.r, dim_color_rgb.g, dim_color_rgb.b);
-
-				this.strokeStyle = dim_color;
 				for(var i = 0, len = text.length; i<len; i++){
 					var ch = text.charCodeAt(i);
 
@@ -138,32 +153,23 @@ var FlynnCanvas = Class.extend({
 						p = FlynnPoints.UNIMPLEMENTED_CHAR;
 					}
 
-					this.beginPath();
-					this.fillStyle = color;
-					//this.lineWidth = "6"; // Fat lines for screenshot thumbnail generation
 					var pen_up = false;
-					var vertex_points = [];
+					this.vectorStart(color);
 					for (var j=0, len2=p.length; j<len2; j+=2){
 						if(p[j]==FlynnPoints.PEN_UP){
 							pen_up = true;
 						}
 						else{
-							vertex_points.push(Math.floor(p[j]*scale+x), Math.floor(p[j+1]*scale +y));
 							if(j===0 || pen_up){
-								this.moveToVec(p[j]*scale+x, p[j+1]*scale +y);
+								this.vectorMoveTo(p[j]*scale+x, p[j+1]*scale +y);
 								pen_up = false;
 							}
 							else{
-								this.lineToVec(p[j]*scale+x, p[j+1]*scale +y);
+								this.vectorLineTo(p[j]*scale+x, p[j+1]*scale +y);
 							}
 						}
 					}
-					this.stroke();
-
-					// Draw the vector vertex points
-					for(j=0, len2=vertex_points.length; j<len2; j+=2) {
-						ctx.fillRect(vertex_points[j], vertex_points[j+1], 1, 1);
-					}
+					this.vectorEnd();
 					x += step;
 				}
 				this.DEBUGLOGGED = true;
