0fc5c0d4d8a6dca49a0d68bf677c06e26010b09d
Eric Moyer <eric@lemoncrab.com>
2015-05-24 20:12:47 -0700

    Cleanup

--- js/stateEnd.js
+++ js/stateEnd.js
@@ -91,7 +91,7 @@ var StateEnd = FlynnState.extend({
 		if (this.hasEnteredName) {
 			// manually tweaked positions for, straightforward text
 			// positioning
-			ctx.vectorText("HALL OF FAME", 4, null, 130, null, EndScreenColor);
+			ctx.vectorText("HIGH SCORES", 4, null, 130, null, EndScreenColor);
 			for (var i = 0, len = this.mcp.highscores.length; i < len; i++) {
 				var hs = this.mcp.highscores[i];
 				ctx.vectorText(hs[0], 2, 390, 200+25*i, null, EndScreenColor);
@@ -101,7 +101,7 @@ var StateEnd = FlynnState.extend({
 
 		} else {
 
-			ctx.vectorText("YOU MADE IT TO THE HALL OF FAME!", 4, null, 100, null, EndScreenColor);
+			ctx.vectorText("YOU MADE IT TO THE HIGH SCORE LIST", 4, null, 100, null, EndScreenColor);
 			ctx.vectorText("TYPE YOUR NAME AND PRESS ENTER", 2, null, 180, null, EndScreenColor);
 			if(this.cursorBlinkTimer%2 > 1){
 				ctx.vectorText(" " + this.nick + "_", 3, null, 220, null, EndScreenColor);
