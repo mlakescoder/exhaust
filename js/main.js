013397953bbbd8ff5f3ebf6728d82e010ec6726f
Eric Moyer <eric@lemoncrab.com>
2017-02-25 18:06:25 -0800

    Adopt howler v2.0.2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var Game = Game || {}; // Create namespace
 
 (function () { "use strict";
 
-Game.VERSION = '1.14';
+Game.VERSION = '1.15';
 Game.CANVAS_HEIGHT = 768;
 Game.CANVAS_WIDTH = 1024;
 Game.SPEED_FACTOR = 0.7;
